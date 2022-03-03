42c086ea242fd4d01d0e227a8de517a81a2fb682
Yongzhou Chen <yongzhouc@outlook.com>
2022-03-03 16:45:12 -0600

    update exp configurations

---
+++ exp-results-beta1/5g-fix10ues-200800-rural/run_simple.sh
@@ -0,0 +1,18 @@
+run_onecase() {
+    slice=$1
+    for i in $(seq 0 0); do
+        let "allue=10*$slice"
+        #../../LTE-Sim SingleCellWithI 3 1 $allue 0 0 1 0 1 1 30 0.1 128 $i ${slice}slices/config 2> ${slice}slices/single_pf$i.log > /dev/null &
+
+        ../../LTE-Sim SingleCellWithI 3 1 $allue 0 0 1 0 7 1 30 0.1 128 $i ${slice}slices/config 2> ${slice}slices/nvs_pf$i.log > /dev/null &
+        ../../LTE-Sim SingleCellWithI 3 1 $allue 0 0 1 0 8 1 30 0.1 128 $i ${slice}slices/config 2> ${slice}slices/greedy_pf$i.log > /dev/null &
+        ../../LTE-Sim SingleCellWithI 3 1 $allue 0 0 1 0 9 1 30 0.1 128 $i ${slice}slices/config 2> ${slice}slices/subopt_pf$i.log > /dev/null &
+        ../../LTE-Sim SingleCellWithI 3 1 $allue 0 0 1 0 10 1 30 0.1 128 $i ${slice}slices/config 2> ${slice}slices/upperbound_pf$i.log > /dev/null
+
+    done
+}
+
+#run_onecase 4 &
+#run_onecase 10
+#run_onecase 20 &
+run_onecase 32
