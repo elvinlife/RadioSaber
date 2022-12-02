b0892006c92cf062f4c2a3ca33abc7c7f8a7969d
Yongzhou Chen <yongzhouc@outlook.com>
2022-12-02 17:22:09 -0600

    add scripts and json configs for exp-customization

---
+++ NSDI23-radiosaber-experiments/exp-customization/run_backlogged.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ODIR="exp-backlogged-20slices"
+#ODIR="exp-backlogged-20slicesdiffw"
+
+run_onecase() {
+    for i in $(seq 0 2); do
+        TRACE_PATH="$HOME/Research/RadioSaber/pbecc-traces-noise0/"
+        cp $TRACE_PATH/mapping$i.config $TRACE_PATH/mapping.config
+
+        ../../LTE-Sim SingleCellWithI 1 1 1 30 $i 12 ${ODIR}/config.json 2> ${ODIR}/single_${i}.log > /dev/null &
+        ../../LTE-Sim SingleCellWithI 1 7 1 30 $i 12 ${ODIR}/config.json 2> ${ODIR}/nvs_${i}.log > /dev/null &
+        ../../LTE-Sim SingleCellWithI 1 9 1 30 $i 12 ${ODIR}/config.json 2> ${ODIR}/maxcell_${i}.log > /dev/null &
+        sleep 1
+    done
+}
+
+run_onecase
