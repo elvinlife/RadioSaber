e11cd0d4507fbd601590e89226d308115453b2fb
Yongzhou Chen <yongzhouc@outlook.com>
2022-12-05 15:45:22 -0600

    add scripts and config files of fix20slices and fixranue experiments

---
+++ NSDI23-radiosaber-experiments/exp-fix20slices/run_exps.sh
@@ -0,0 +1,18 @@
+# ODIR="ues"
+ODIR="ues-ip"
+
+ueArray=("5" "10" "15" "20")
+
+run_onecase() {
+    i=$1
+    for ue in ${ueArray[@]}; do
+        ../../LTE-Sim SingleCellWithI 1 7 1 30 $i 12 ${ue}${ODIR}/config-${2}.json 2> ${ue}${ODIR}/nvs_${2}$i.log > /dev/null &
+        ../../LTE-Sim SingleCellWithI 1 9 1 30 $i 12 ${ue}${ODIR}/config-${2}.json 2> ${ue}${ODIR}/maxcell_${2}$i.log > /dev/null &
+        # ../../LTE-Sim SingleCellWithI 1 8 1 30 $i 12 ${ue}${ODIR}/config-${2}.json 2> ${ue}${ODIR}/sequential_${2}$i.log > /dev/null &
+        # ../../LTE-Sim SingleCellWithI 1 10 1 30 $i 12 ${ue}${ODIR}/config-${2}.json 2> ${ue}${ODIR}/upperbound_${2}$i.log > /dev/null &
+    done
+}
+
+TRACE_PATH="$HOME/Research/RadioSaber/pbecc-traces-noise0/"
+cp $TRACE_PATH/mapping0.config $TRACE_PATH/mapping.config
+run_onecase 0 pf
---
+++ NSDI23-radiosaber-experiments/exp-fixranues/run_exps.sh
@@ -0,0 +1,16 @@
+#ODIR="slices"
+ODIR="slices-ip"
+
+sliceArray=("5" "10" "15" "20")
+
+run_onecase() {
+    i=$1
+    for n in ${sliceArray[@]}; do
+        ../../LTE-Sim SingleCellWithI 1 7 1 30 $i 12 ${n}${ODIR}/config-${2}.json 2> ${n}${ODIR}/nvs_${2}$i.log > /dev/null &
+        ../../LTE-Sim SingleCellWithI 1 9 1 30 $i 12 ${n}${ODIR}/config-${2}.json 2> ${n}${ODIR}/maxcell_${2}$i.log > /dev/null &
+    done
+}
+
+TRACE_PATH="$HOME/Research/RadioSaber/pbecc-traces-noise0/"
+cp $TRACE_PATH/mapping0.config $TRACE_PATH/mapping.config
+run_onecase 0 pf
