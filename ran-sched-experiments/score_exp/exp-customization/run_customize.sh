22bbfa063d6b3b93528f18ee2e705b05ac650980
PeterYaoNYU <yy4108@nyu.edu>
2023-11-10 13:02:52 +0000

    score exp setup

---
+++ ran-sched-experiments/score_exp/exp-customization/run_customize.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ODIR="exp-customize-20slices"
+
+run_onecase() {
+    for i in $(seq 0 2); do
+        TRACE_PATH="../../../cqi-traces-noise0"
+        cp $TRACE_PATH/mapping$i.config $TRACE_PATH/mapping.config
+
+        # ../../../LTE-Sim SingleCellWithI 1 1 1 30 $i 40 ${ODIR}/config.json 2> ${ODIR}/single_${i}.log > /dev/null &
+        # ../../../LTE-Sim SingleCellWithI 1 7 1 30 $i 40 ${ODIR}/config.json 2> ${ODIR}/nvs_${i}.log > /dev/null &
+        ../../../LTE-Sim SingleCellWithI 1 9 1 30 $i 40 ${ODIR}/config.json 2> ${ODIR}/max_throughput_${i}.log > /dev/null &
+
+        ../../../LTE-Sim SingleCellWithI 1 91 1 30 $i 40 ${ODIR}/config.json 2> ${ODIR}/pf_${i}.log > /dev/null &
+        ../../../LTE-Sim SingleCellWithI 1 92 1 30 $i 40 ${ODIR}/config.json 2> ${ODIR}/mlwdf_${i}.log > /dev/null &
+
+        sleep 1
+    done
+}
+
+run_onecase
