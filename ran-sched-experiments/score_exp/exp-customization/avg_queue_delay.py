920622185902c55526c8309028700788cf7bc640
PeterYaoNYU <yy4108@nyu.edu>
2023-11-11 16:51:26 +0000

    add plotting to the hol analysis python script

--- ran-sched-experiments/score_exp/exp-customization/avg_queue_delay.py
+++ ran-sched-experiments/score_exp/exp-customization/avg_queue_delay.py
@@ -1,4 +1,6 @@
 import re
+import matplotlib.pyplot as plt
+
 
 def calculate_average_hol_delay(log_filename):
     # Regular expression pattern to match lines in the log file
@@ -31,15 +33,51 @@ def calculate_average_hol_delay(log_filename):
 
 # Example usage
 print("PF")
+pf_hol=[]
 log_filename = './exp-customize-20slices/pf_1.log'
-calculate_average_hol_delay(log_filename)
+average_hol_delays_pf = calculate_average_hol_delay(log_filename)
+for slice_id in range(10, 15):
+    pf_hol.append(average_hol_delays_pf[slice_id])
 
 print("MT")
+mt_hol=[]
 log_filename = './exp-customize-20slices/max_throughput_1.log'
-calculate_average_hol_delay(log_filename)
+average_hol_delays_mt = calculate_average_hol_delay(log_filename)
+for slice_id in range(10, 15):
+    mt_hol.append(average_hol_delays_mt[slice_id])
+
+
+
 
 print("mlwdf")
+mlwdf_hol=[]
 log_filename = './exp-customize-20slices/mlwdf_1.log'
-calculate_average_hol_delay(log_filename)
+average_hol_delays_mlwdf = calculate_average_hol_delay(log_filename)
+for slice_id in range(10, 15):
+    mlwdf_hol.append(average_hol_delays_mlwdf[slice_id])
+    
+    
+
+print(pf_hol)
+print(mt_hol)
+print(mlwdf_hol)
+
+slices = range(10, 15)
+bar_width = 0.2
+x = [slice_id + bar_width for slice_id in slices]
+
+
+plt.figure(figsize=(15, 8))
+
+plt.bar([slice_id - bar_width for slice_id in slices], pf_hol, width=bar_width, label='PF')
+plt.bar(slices, mt_hol, width=bar_width, label='MT')
+plt.bar([slice_id + bar_width for slice_id in slices], mlwdf_hol, width=bar_width, label='MLWDF')
+
+plt.xlabel('Slice Index')
+plt.ylabel('Average HOL Delay')
+plt.title('Average HOL Delay for different Interslice algo')
+plt.legend()
+plt.savefig(f'./exp-customize-20slices_second_run/hol_delay_comparison.png')
+plt.close()
 
 
